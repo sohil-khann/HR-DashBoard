d90d0345b798c69ba63f8269a60f426da52d1bbd
sohil-khann <khaa40408@gmail.com>
2025-07-20 22:22:39 +0530

    [Sohil khan ] Add. employee

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,18 @@
+
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
+
 import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: "--font-geist-sans", 
+  subsets: ["latin"], 
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-geist-mono", 
+  subsets: ["latin"], 
 });
 
 export const metadata = {
@@ -20,12 +23,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
+      {/* Apply fonts and basic styling to body */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-full flex flex-col`}
       >
+        {/* Add navigation bar at the top */}
         <Navbar />
+        {/* Main content area that grows to fill available space */}
         <main className="flex-grow">
-          {children}
+          {children} {/* Display page content here */}
         </main>
       </body>
     </html>
